656a5c5eb1465cdaf4ca4ad1a6946cb48cecf94f
IanMcT <ian.mctavish@tldsb.on.ca>
2017-04-27 09:58:45 -0400

    Update startScreen.cs
    Added button events

--- SMG/SMG/startScreen.cs
+++ SMG/SMG/startScreen.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,97 @@ namespace SMG
         public startScreen()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Difficulty_Easy_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Difficulty_Medium_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Difficulty_Hard_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Difficulty_VeryHard_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Length_Seven_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        ///  easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Length_Fourteen_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Length_Twentyone_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        ///  easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Length_Endless_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Cancel_Game_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// easy button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Game_Click(object sender, EventArgs e)
+        {
+
         }
 
 
