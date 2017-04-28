7b4b54786b36039cae1f5e563abe8b2c6252a976
IanMcT <ian.mctavish@tldsb.on.ca>
2017-04-28 09:56:07 -0400

    Update Stock.cs
    Fixed code in stock.cs

--- SMG/SMG/Stock.cs
+++ SMG/SMG/Stock.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,27 @@ using System.Threading.Tasks;
 namespace SMG
 {
     class Stock
-    {
+    { 
+       /// <summary>
+       /// Name of the Stock
+       /// </summary>
+        public string StockName;
+        /// <summary>
+        /// Description of the Stock
+        /// </summary>
+        public string StockDescription;
+       /// <summary>
+       /// The last four or so points for the Stock
+       /// </summary>
+        public decimal StockHistory;  
+        /// <summary>
+        /// scale of one to ten determining if its going to be low risk or high risk
+        /// </summary>
+        public decimal StockRisk;
+        /// <summary>
+        /// what percent payout the stock is going to have
+        /// </summary>
+        public decimal StockReturn;
+
     }
 }
