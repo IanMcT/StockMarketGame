c6ffda639d4d8dc215554bb75f627fa431f958d2
IanMcT <ian.mctavish@tldsb.on.ca>
2017-05-04 15:00:42 -0400

    Update Program.cs

--- SMG/SMG/Program.cs
+++ SMG/SMG/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ namespace SMG
 {
     static class Program
     {
+        public static User user;
+        public static User difficulty;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +18,9 @@ namespace SMG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var MainForm = new Form1();
+            MainForm.Show();
+            Application.Run();
         }
     }
 }
