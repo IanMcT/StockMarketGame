90dba3e87e59a5acab24609490546986fcb72397
IanMcT <ian.mctavish@tldsb.on.ca>
2017-05-04 14:59:40 -0400

    Update Stock.cs

--- SMG/SMG/Stock.cs
+++ SMG/SMG/Stock.cs
@@ -7,27 +7,31 @@ using System.Threading.Tasks;
 namespace SMG
 {
     class Stock
-    { 
-       /// <summary>
-       /// Name of the Stock
-       /// </summary>
+    {
+        /// <summary>
+        /// Name of the Stock
+        /// </summary>
         public string StockName;
         /// <summary>
         /// Description of the Stock
         /// </summary>
         public string StockDescription;
-       /// <summary>
-       /// The last four or so points for the Stock
-       /// </summary>
-        public decimal StockHistory;  
+        /// <summary>
+        /// The last four or so points for the Stock
+        /// </summary>
+        public double StockHistory;
         /// <summary>
         /// scale of one to ten determining if its going to be low risk or high risk
         /// </summary>
-        public decimal StockRisk;
+        public double StockRisk;
         /// <summary>
         /// what percent payout the stock is going to have
         /// </summary>
-        public decimal StockReturn;
+        public double StockReturn;
 
+        public Stock(string StockName)
+        {
+            this.StockName = StockName;
+        }
     }
 }
